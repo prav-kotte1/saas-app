bf4b7ac6e19e512f51ba43a7648c4bb87cebc4e2
Pravallika Kotte <146621306+prav-kotte1@users.noreply.github.com>
2025-06-21 12:11:44 +0530

    .

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,9 +1,7 @@
-import React from "react";
+import { SignIn } from '@clerk/nextjs'
 
-const Signin = () => {
-    return (
-        <div>Sign in</div>
-    )
+export default function Page() {
+    return <main className="flex items-center justify-center">
+        <SignIn />
+    </main>
 }
-
-export default Signin;
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
+import {PricingTable} from "@clerk/nextjs";
 
 const Subscription = () => {
-  return (
-    <div>
-      Subscription
-    </div>
-  );
-};
-
-export default Subscription;
+    return (
+        <main>
+            <PricingTable />
+        </main>
+    )
+}
+export default Subscription
